eaf8ca617b3f25674e956bbc621a1666be882c95
Alain Mauri <alain.mauri@thefloow.com>
2020-06-08 18:19:23 +0100

    Fix failing test for Influx client

--- spec/lib/easy_monitor/influxdb/client_spec.rb
+++ spec/lib/easy_monitor/influxdb/client_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 module EasyMonitor
   module Influxdb
     RSpec.describe Client do
+      let(:influxdb_client) { described_class.new(time_precision: 'ms') }
       context 'when connecting' do
-        let(:influxdb_client) { described_class.new }
-
         it 'returns a client' do
           expect(influxdb_client.is_a?(EasyMonitor::Influxdb::Client)).to eq(true)
         end
@@ -21,7 +20,6 @@ module EasyMonitor
 
       context "when writing" do
         describe "#write" do
-          let(:influxdb_client) { described_class.new }
           let(:database) { 'easy_monitor' }
           let(:tags) { { type: 'test' } }
           let(:values) { { value: 'my value' } }
@@ -33,7 +31,7 @@ module EasyMonitor
 
           before do
             stub_request(:post, 'http://localhost:8086/write').with(
-              query: { u: 'root', p: 'root', precision: 's', db: database},
+              query: { u: 'root', p: 'root', precision: 'ms', db: database},
               headers: {
                 'Accept' => '*/*',
                 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
@@ -41,7 +39,7 @@ module EasyMonitor
                 'User-Agent' => 'Ruby'
               },
               body: body
-            ).to_return(status: 200, body: "", headers: {})
+            ).to_return(status: 204)
           end
 
           it "returns a 204 after writing" do
